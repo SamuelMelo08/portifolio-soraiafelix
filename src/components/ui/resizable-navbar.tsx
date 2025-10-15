f80b30e279ada723fb658112f33b0ace216dd08f
Samuel Melo <samuelmelo13789@gmail.com>
2025-10-15 08:58:48 -0300

    navbar image adjustments

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -7,6 +7,7 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "motion/react";
+import Image from "next/image";
 
 import React, { useRef, useState } from "react";
 
@@ -229,7 +230,7 @@ export const NavbarLogo = () => {
       href="#"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
-      <img
+      <Image
         src="/06.png"
         alt="logo"
         width={150}
